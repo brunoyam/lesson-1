dde1b26beab31d022604615ab56b2f79915ddf3d
brunoyam <striter94@yandex.ru>
2020-06-15 18:51:57 +0300

    changes

--- src/com/brunoyam/rakovskiy/lesson1/Main.java
+++ src/com/brunoyam/rakovskiy/lesson1/Main.java
@@ -3,6 +3,7 @@ package com.brunoyam.rakovskiy.lesson1;
 public class Main {
 
     public static void main(String[] args) {
+        // Пример простейшей программы
         System.out.println("Hello, Brunoyam!");
     }
 }
